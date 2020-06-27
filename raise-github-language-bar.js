796f870304af0627831896430b6236356cbc230d
Dennis Schüßler <github.ds@web.de>
2020-06-27 21:08:51 +0200

    Improves script so that it also works when navigating to repo from within Github

--- raise-github-language-bar.js
+++ raise-github-language-bar.js
@@ -3,16 +3,24 @@
 // @version 1.0
 // @description Raises the language bar in Github so that it is visible in the viewport without scrolling
 // @include https://github.com/*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
+// @require https://gist.github.com/raw/2625891/waitForKeyElements.js
 // ==/UserScript==
 
-const borderGridRows = document.getElementsByClassName("BorderGrid-row");
-const lastRow = borderGridRows.item(borderGridRows.length - 1);
+waitForKeyElements(".BorderGrid", reorderGridRows);
 
-if (
-  lastRow
-    .getElementsByClassName("BorderGrid-cell")[0]
-    .getElementsByTagName("h2")[0].textContent === "Languages"
-) {
-  borderGridRows[0].parentNode.removeChild(lastRow);
-  borderGridRows[0].parentNode.insertBefore(lastRow, borderGridRows[1]);
+function reorderGridRows(grid) {
+  const gridRows = Object.values(grid.children()).filter((child) =>
+    child?.attributes?.class?.nodeValue.includes("BorderGrid-row")
+  );
+  const lastRow = gridRows[gridRows.length - 1];
+
+  if (
+    lastRow
+      .getElementsByClassName("BorderGrid-cell")[0]
+      .getElementsByTagName("h2")[0].textContent === "Languages"
+  ) {
+    gridRows[0].parentNode.removeChild(lastRow);
+    gridRows[0].parentNode.insertBefore(lastRow, gridRows[1]);
+  }
 }
